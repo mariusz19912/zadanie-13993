7377e123ce7a2760331abe1e1b2e74cef7aa334a
Mariusz Betkier <mariusz.betkier11@gmail.com>
2019-01-23 23:18:49 +0100

    zadanie 13993

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function porownanie(a,b) {
+    var c = a+b;
+    if (a==20) {
+        console.log("True");
+    } else if (b==20) {
+        console.log("True");
+    } else if (c<=20) {
+        console.log("True");
+    }else{
+        console.log("False");
+    };
+
+};
+porownanie(11,10);
