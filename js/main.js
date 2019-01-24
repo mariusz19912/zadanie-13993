13c5b7b2fae31de759303ae16d878d19cf2938f3
Mariusz Betkier <mariusz.betkier11@gmail.com>
2019-01-24 22:13:09 +0100

    poprawione

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 function porownanie(a,b) {
     var c = a+b;
-    if (a==20) {
-        console.log("True");
-    } else if (b==20) {
-        console.log("True");
-    } else if (c<=20) {
-        console.log("True");
-    }else{
-        console.log("False");
-    };
+    if (a==20 || b==20 || c<=20 ) {
+       console.log(Boolean(a));
+    } 
+  
 
 };
 
-porownanie(11,10);
+porownanie(15,10);
