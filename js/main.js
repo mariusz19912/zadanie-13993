942d397c3af15196f2fad845f97a84e64fdc9b62
Mariusz Betkier <mariusz.betkier11@gmail.com>
2019-01-28 22:31:40 +0100

    zmienione na return

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function porownanie(a,b) {
     var c = a+b;
     if (a==20 || b==20 || c<=20 ) {
-       console.log(Boolean(a));
-    } 
-  
-
+       return true;
+    } else{
+        return false;
+    };
 };
 
 porownanie(15,10);
